6881bfe769c81aa7d4b8b3e2ee224c10e57ae97b
m salman a <mohammadalfarisi6@gmail.com>
2022-06-08 12:18:17 +0700

    Initial Commit

---
+++ MessageUIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageUIDemo
+//
+//  Created by Mohammad Alfarisi on 08/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
